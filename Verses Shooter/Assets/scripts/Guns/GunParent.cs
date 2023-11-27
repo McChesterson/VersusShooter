92d4f3e23f425370dfed1cd8fcca1b1d357e0684
McChesterson <o.a.onstott@gmail.com>
2023-11-26 20:28:55 -0800

    Added GunParent script

---
+++ Verses Shooter/Assets/scripts/Guns/GunParent.cs
@@ -0,0 +1,47 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunParent : MonoBehaviour
+{
+    public int damage;
+    public float fireRate;
+    public float range;
+    [Space]
+    public bool automatic;
+    [Space]
+    public Camera cam;
+
+    private float nextFire;
+
+    void Update()
+    {
+        if (nextFire > 0)
+        {
+            nextFire -= Time.deltaTime;
+        }
+        if (automatic ? Input.GetButton("fire1") : Input.GetButtonDown("fire1"))
+        {
+            nextFire = 1 / fireRate;
+
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, range))
+        {
+            if (hit.transform.gameObject.GetComponent<Health>())
+            {
+                hit.transform.gameObject.GetComponent<PhotonView>().RPC("TakeDamage", RpcTarget.All, damage);
+
+            }
+        }
+    }
+}
