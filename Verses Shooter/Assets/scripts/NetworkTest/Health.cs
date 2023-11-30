9ee68064535e0e4c92cb58a9ebf558c2e6ee0865
McChesterson <o.a.onstott@gmail.com>
2023-11-29 18:31:01 -0800

    fixed networking and health bar

--- Verses Shooter/Assets/scripts/NetworkTest/Health.cs
+++ Verses Shooter/Assets/scripts/NetworkTest/Health.cs
@@ -9,20 +9,25 @@ public class Health : MonoBehaviour
 {
     public int maxHealth = 100;
     int currentHealth = 100;
+    float fillValue = 1f;
 
     public Image hpBar;
+
 
     [PunRPC]
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
-        hpBar.fillAmount -= damage / maxHealth;
+
+        fillValue -= damage / (float)maxHealth;
+
+        hpBar.fillAmount = fillValue;
 
         if (currentHealth <= 0)
         {
             Destroy(gameObject);
         }
         Debug.Log("took " + damage + " damage : " + currentHealth);
-        Debug.Log("fillAmount: " + hpBar.fillAmount);
+        Debug.Log("fillAmount: " + fillValue);
     }
 }
